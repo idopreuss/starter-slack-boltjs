debbbfed11cf9ba89bea707a361685182516b3b2
Ido Preuss <ido.preuss@tipalti.com>
2022-12-20 22:06:33 +0200

    Restore

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
-const SlackBot = require('slackbots');
 
-const bot = new SlackBot({
-token: 'YOUR_SLACK_BOT_TOKEN',
-name: 'My Bot'
+const express = require('express')
+const { App, ExpressReceiver } = require('@slack/bolt');
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
+receiver.router.use(express.static('public'))
+const app = new App({
+  receiver,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
-
-// Start the bot
-bot.on('start', () => {
-console.log('Bot has started');
+app.message(':wave:', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`,
+    text: `Hey there there <@${message.user}>!`,
+    thread_ts: message.ts
+  });
 });
-
-// Message handler
-bot.on('message', data => {
-// Check if the message is from a bot
-if (data.subtype && data.subtype === 'bot_message') return;
-
-// Get the user who sent the message
-const user = data.user;
-
-// Get the message text
-const message = data.text;
-
-// Do something with the message here
-console.log( "Received message from ${user}: ${message}");
+app.event('app_mention', async ({ event, say }) => {
+  await say({
+    text: `Hello <@${event.user}>: If you send me a :wave: I'll send you a button to click. If you add a reaction to a message I'll say thanks.`,
+    thread_ts: event.ts
+  })
 });
+app.event('reaction_added', async ({ event, say }) => {
+  await say({
+    text: `Thanks for the :${event.reaction}:`,
+    thread_ts: event.item.ts
+  })
+});
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  // console.log(JSON.stringify(body,null,2))
+  await say({
+    text: `<@${body.user.id}> you clicked the button. Well done.`,
+    thread_ts: body.message.ts
+  });
+});
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
