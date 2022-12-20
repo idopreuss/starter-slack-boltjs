adf7e985110512597190fa7cae9ee6ca3d37c6b7
Ido Preuss <ido.preuss@tipalti.com>
2022-12-20 13:57:11 +0200

    new app

--- app.js
+++ app.js
@@ -1,73 +1,11 @@
-const express = require('express')
-const { App, ExpressReceiver } = require('@slack/bolt');
-const app2 = express();
-const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
+const express = require('express');
+const app = express();
 
-receiver.router.use(express.static('public'))
-
-const app = new App({
-  receiver,
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+app.get('/ido', function(req, res) {
+  res.send('Hello Wo rld!');
 });
 
-
-app.message(':wave:', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ],
-    text: `Hey there there <@${message.user}>!`,
-    thread_ts: message.ts
-  });
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Web service listening on port 8080');
 });
 
-app.event('app_mention', async ({ event, say }) => {
-  await say({
-    text: `Hello <@${event.user}>: If you send me a :wave: I'll send you a button to click. If you add a reaction to a message I'll say thanks.`,
-    thread_ts: event.ts
-  })
-});
-
-app.event('reaction_added', async ({ event, say }) => {
-  await say({
-    text: `Thanks for the :${event.reaction}:`,
-    thread_ts: event.item.ts
-  })
-});
-
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  // console.log(JSON.stringify(body,null,2))
-  await say({
-    text: `<@${body.user.id}> you clicked the button. Well done.`,
-    thread_ts: body.message.ts
-  });
-});
-
-(async () => {
-  // Start the app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
-
-app2.get('/ido', function(req, res) {
-  res.send('Helloo  World!');
-});
