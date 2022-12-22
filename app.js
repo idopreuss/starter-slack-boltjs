11e7b3484b8056185fb36f2fa17786f070b16605
Ido Preuss <ido.preuss@tipalti.com>
2022-12-22 15:22:04 +0200

    new app

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 
-const { App, SocketModeReceiver } = require('@slack/bolt');
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
 
 
 // const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
 // receiver.router.use(express.static('public'))
-
-const socketModeReceiver = new SocketModeReceiver({
-  appToken: process.env.SLACK_APP_TOKEN,
-});
-
-const app = new App({
-  socketModeReceiver,
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
-});
 
 
 slackBody = {
