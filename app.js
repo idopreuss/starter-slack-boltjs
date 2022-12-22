c3f54c0e2fc4ed0e66328f6c90d0588688e44844
Ido Preuss <ido.preuss@tipalti.com>
2022-12-22 17:40:24 +0200

    remove port

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.command('/room', async ({ say }) => {
 
 (async () => {
   // Start the app
-  await app.start(process.env.PORT || 3000);
+  await app.start();
   console.log('⚡️ Bolt app is running!');
 })();
